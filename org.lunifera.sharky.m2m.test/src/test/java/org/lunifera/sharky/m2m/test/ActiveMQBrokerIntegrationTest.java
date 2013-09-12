ec5e3afe7b3889befae116299e584e233be11698
Peter Kofler <peter.kofler@code-cop.org>
2013-09-12 14:19:58 +0200

    ActiveMQ is running

---
+++ org.lunifera.sharky.m2m.test/src/test/java/org/lunifera/sharky/m2m/test/ActiveMQBrokerIntegrationTest.java
@@ -0,0 +1,16 @@
+package org.lunifera.sharky.m2m.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ActiveMQBrokerIntegrationTest {
+
+	@Test
+	public void test() {
+	}
+
+}
